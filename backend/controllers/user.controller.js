6d049d76ccbd25da241fa74a4baa09a649af7461
thesawankumar <saankushwaha249@gmail.com>
2024-11-30 17:56:46 +0530

    users login done

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -18,3 +18,21 @@ module.exports.registerUser = async (req, res) => {
   const token = user.generateAuthToken();
   res.status(201).json({ user, token });
 };
+
+module.exports.loginUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+  const isValidPassword = await user.comparePassword(password);
+  if (!isValidPassword) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+  const token = user.generateAuthToken();
+  res.status(200).json({ user, token });
+};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,32 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controller");
-router.post("/register", [
+router.post(
+  "/register",
+  [
     body("email").isEmail().withMessage("Invalid email"),
-    body("fullname.firstname").isLength({ min: 3 }).withMessage("First name must be at least 3 characters long"),
-    body("fullname.lastname").isLength({ min: 3 }).withMessage("Last name must be at least 3 characters long"),
-    body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),],
-    userController.registerUser);
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First name must be at least 3 characters long"),
+    body("fullname.lastname")
+      .isLength({ min: 3 })
+      .withMessage("Last name must be at least 3 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.registerUser
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  userController.loginUser
+);
+
 module.exports = router;
