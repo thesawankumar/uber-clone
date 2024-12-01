8f0cc2a122855ffd001351dd0897ddadf1eebc7d
thesawankumar <saankushwaha249@gmail.com>
2024-12-01 10:55:16 +0530

    Context api done

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useState } from "react";
+
+/* eslint-disable react/prop-types */
+export const UserDataContext = createContext();
+
+const UserContext = ({ children }) => {
+  const [user, setUser] = useState({
+    email: "",
+    fullName: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+  return (
+    <div>
+      <UserDataContext.Provider value={{ user, setUser }}>
+        {children}
+      </UserDataContext.Provider>
+    </div>
+  );
+};
+
+export default UserContext;
